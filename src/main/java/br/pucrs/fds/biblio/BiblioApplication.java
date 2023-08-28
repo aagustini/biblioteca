d1eaf90671e67b5cc829d3a806baf1fba04b81df
Alexandre Agustini <alexandre.agustini@pucrs.br>
2023-08-28 14:38:59 -0300

    Exemplo springboot

---
+++ src/main/java/br/pucrs/fds/biblio/BiblioApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.fds.biblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioApplication.class, args);
+	}
+
+}
